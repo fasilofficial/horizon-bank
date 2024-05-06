b0ea4945ad82680d5139362d486774d5bacb9e20
fasilofficial <muhammedfasiloffcial@gmail.com>
2024-05-06 15:49:54 +0530

    Implement pagination

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from "@/components/HeaderBox";
+import { Pagination } from "@/components/Pagination";
 import TransactionsTable from "@/components/TransactionsTable";
 import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
@@ -16,11 +17,19 @@ const TransactionHistory = async ({
 
   if (!accounts) return;
 
-  const accountsData = accounts?.data;
-
   const appwriteItemId = (id as string) || accounts?.data[0]?.appwriteItemId;
 
   const account = await getAccount({ appwriteItemId });
+
+  const rowsPerPage = 10;
+  const totalPages = Math.ceil(account?.transactions.length / rowsPerPage);
+  const indexOfLastTransaction = currentPage * rowsPerPage;
+  const indexOfFirstTransaction = indexOfLastTransaction - rowsPerPage;
+
+  const currentTransactions = account?.transactions.slice(
+    indexOfFirstTransaction,
+    indexOfLastTransaction
+  );
 
   return (
     <section className="transactions">
@@ -46,12 +55,20 @@ const TransactionHistory = async ({
 
           <div className="transactions-account-balance">
             <p className="text-14">Current Balance</p>
-            <p className="text-24 text-center font-bold">{formatAmount(account?.data.currentBalance)}</p>
+            <p className="text-24 text-center font-bold">
+              {formatAmount(account?.data.currentBalance)}
+            </p>
           </div>
         </div>
 
         <section className="flex w-full flex-col gap-6">
-          <TransactionsTable transactions={account?.transactions} />
+          <TransactionsTable transactions={currentTransactions} />
+
+          {totalPages > 1 && (
+            <div className="my-4 w-full">
+              <Pagination page={currentPage} totalPages={totalPages} />
+            </div>
+          )}
         </section>
       </div>
     </section>
--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import BankCard from "./BankCard";
+import { countTransactionCategories } from "@/lib/utils";
+import Category from "./Category";
 
 const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
+  const categories: CategoryCount[] = countTransactionCategories(transactions);
+
   return (
     <aside className="right-sidebar">
       <section className="flex flex-col pb-8">
@@ -15,7 +19,9 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
             </span>
           </div>
           <div className="profile-details">
-            <h1 className="profile-name">{user?.firstName} {user?.lastName}</h1>
+            <h1 className="profile-name">
+              {user?.firstName} {user?.lastName}
+            </h1>
             <p className="profile-email">{user?.email}</p>
           </div>
         </div>
@@ -51,6 +57,16 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
             )}
           </div>
         )}
+
+        <div className="mt-10 flex flex-1 flex-col gap-6">
+          <h2 className="header-2">Top categories</h2>
+
+          <div className="space-y-5">
+            {categories.map((category, index) => (
+              <Category key={category.name} category={category} />
+            ))}
+          </div>
+        </div>
       </section>
     </aside>
   );
